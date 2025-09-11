d3297551b55fb95cfd47306df1682230136a78a1
BeoGonzalez <beo.gonzalez@duocuc.cl>
2025-09-11 08:51:28 -0300

    Hice el menú

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,39 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+registroFrutas(){
+    Fruta(nombre: String, precioPorKilo: Int, stockKilos: Int){
+
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fun mostrarMenu(){
+    println("Bienvenido al sistema de gestión de inventario Puerto Frutal SPA")
+    println("1. Registrar nueva fruta (con validaciones completas)")
+    println("2. Mostrar todas las frutas")
+    println("3. Mostrar frutas caras > $1000")
+    println("4. Calcular métricas del inventario (valor total, promedios, extremos)")
+    println("5. Salir")
+}
+
+
+
+fun main() {
+    mostrarMenu()
+
+
 }
